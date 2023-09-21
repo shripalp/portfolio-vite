f4f4c234d532f8f2c9c1691ca0aaf952ea7005b4
shripal <shripalp@gmail.com>
2023-09-20 20:50:38 -0600

    main page completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,56 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import {
+  FaTwitter,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
 
 function Main() {
   return (
     <div id="main">
       <img
-        className=""
+        className="w-full h-screen object-cover object-left scale-x-[-1]"
         src="https://images.unsplash.com/photo-1689361806203-41b00e7227b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1976&q=80"
         alt=""
       />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+            I'm Shripal Parikh
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+            I'm a
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "QA analyst",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Developer",
+                1000,
+                "Yoga Teacher",
+                1000,
+                "Biomedical Technologist",
+                1000,
+              ]}
+              wrapper="div"
+              speed={50}
+              style={{
+                fontSize: "1em",
+                paddingLeft: "5px",
+              }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <FaTwitter className="cursor-pointer" size={20} />
+            <FaFacebookF className="cursor-pointer" size={20} />
+            <FaInstagram className="cursor-pointer" size={20} />
+            <FaLinkedinIn className="cursor-pointer" size={20} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
