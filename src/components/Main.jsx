bb22025926860d172c3d87a932ec915eae08b6b2
shripal <shripalp@gmail.com>
2023-09-20 18:02:22 -0600

    main page added

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div id="main">
+      <img
+        className=""
+        src="https://images.unsplash.com/photo-1689361806203-41b00e7227b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1976&q=80"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Main;
