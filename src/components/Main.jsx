292235b33b0d1d329acd8a1116272b738cc8acd0
shripal <shripalp@gmail.com>
2023-12-07 15:47:34 -0700

    error resolved

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,7 @@ function Main() {
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
           <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
-            I'm Shripal Parikh
+            I am Shripal Parikh
           </h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
             I am a
